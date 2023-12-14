dc7604b0e0e704163aae055865ee66cbac1d443c
dcigic92 <dinocigic@yahoo.com>
2023-12-14 01:38:57 +0000

    Fix allowed hosts

--- codestar2021/settings.py
+++ codestar2021/settings.py
@@ -31,8 +31,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['codestar2021.herokuapp.com', 
-                 '127.0.0.1']
+ALLOWED_HOSTS = ['django-blog-dcigic92-abade6916835.herokuapp.com']
 
 
 # Application definition
